ceaf9a646d8699caa4915b40ba65a21c2efaaee9
RyanYchihara <ryanbeijoychihara@gmail.com>
2026-03-05 22:44:35 -0300

    exerciciosjava1

---
+++ Desenvolvimento-de-Sistemas/projeto/src/projeto/Projeto.java
@@ -0,0 +1,22 @@
+
+package projeto;
+
+
+import java.util.Scanner;
+
+
+public class Projeto {
+
+    public static void main(String[] args) {
+        int valor;
+        Scanner teclado;
+       teclado = new Scanner(System.in);
+        System.out.println("Digite um numero inteiro: ");
+        valor = teclado.nextInt();
+        System.out.println("valor lido: " + valor);
+       
+               
+    }
+    
+}
+                                                                                                                                                                                                                                                                                                                                                                   
